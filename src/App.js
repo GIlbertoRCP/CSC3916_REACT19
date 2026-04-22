69552dc93850b13efe2705247d77602156d782ed
Gilberto Romero-Cano <gilbertorcp712@gmail.com>
2026-04-22 01:43:44 -0600

    Fixed type bug

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import './App.css';
 import MovieHeader from './components/movieheader';
 import MovieList from './components/movielist';
-import Movie from './components/movie';
+import MovieDetail from './components/moviedetail'; // <-- CHANGED THIS LINE
 import Authentication from './components/authentication';
 import {HashRouter, Routes,  Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HashRouter> {/* The Router component */}
+      <HashRouter>
         <MovieHeader />
         <Routes>
           <Route path="/" element={<MovieList />} />
           <Route path="/movielist" element={<MovieList />}/>
-          <Route path="/movie/:movieId" element={<Movie />}/>
+          
+          {/* CHANGED THIS LINE TO USE THE FIXED COMPONENT */}
+          <Route path="/movie/:movieId" element={<MovieDetail />}/> 
+          
           <Route path="/signin" element={<Authentication />}/>
           {/*... other routes */}
         </Routes>
